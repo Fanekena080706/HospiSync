9df47da3609b4b69e513f2c03d7e2a6810d64788
fanekena randrianantenaina <fanekenarandrianantenaina@gmail.com>
2026-07-06 18:54:47 +0300

    modification de l'alborescence de page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Dashboard from "./pages/Dashboard/Dashboard.jsx";
 import "./App.css";
 
 function App() {
